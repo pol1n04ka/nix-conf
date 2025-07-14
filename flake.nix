20be426a3fd2e9d374bcb292f535339ccef030e7
Polina Prokopenko <topolina1337@gmail.com>
2025-07-14 21:42:27 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem { 
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/laptop/configuration.nix
+      ];
+    };
+  };
+}
